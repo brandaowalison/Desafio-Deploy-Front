5a1eda4d5826330ea188782c189c9ca8733523cb
Walison Brandão <walisonbrandao30@gmail.com>
2025-03-25 15:32:41 -0300

    master

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://desafio-deploy-back.onrender.com/api/complaints';
+const API_URL = 'https://desafio-deploy-back.onrender.com/api/complaints/';
 
 document.getElementById('complaintForm').addEventListener('submit', async (e) => {
     e.preventDefault();
